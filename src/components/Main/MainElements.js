9ad4f5a65425d4bbfee66b32ec138f33df8e1569
Majick333 <rmccauleycode@gmail.com>
2021-06-20 10:54:10 -0500

    created buttonElement

--- src/components/Main/MainElements.js
+++ src/components/Main/MainElements.js
@@ -10,6 +10,19 @@ export const MainContainer = styled.div`
     position: relative;
     z-index: 1;
     font-family: 'fira code', monospace;
+
+        :before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0; 
+            right: 0;
+            bottom: 0;
+            background: linear-gradient(180deg, rgba(0,0,0, 0.2) 0%, rgba(0,0,0, 0.7) 100%);
+            linear-gradient(180deg, rgba(0,0,0,0.2) 0%, transparent 100%);
+            z-index: 2;
+
+        }
 `;
 
 export const MainBackGround = styled.div`
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,23 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 import  Video from '../../videos/video.mp4';
+import {Button} from '../ButtonElements';
 import { MainContainer, MainBackGround, VideoBackground, MainContent, MainHeader, MainP } from './MainElements';
 
 const MainSection = () => {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = () =>{
+        setHover(!hover);
+    }
     return (
         <MainContainer>
             <MainBackGround>
                 <VideoBackground autoPlay loop muted src={Video} type='video/mp4' />
             </MainBackGround>
-
                 <MainContent>
                     <MainHeader> Ryan McCauley </MainHeader>
                     <MainP> Musician, Developer, Tinkerer </MainP>
+                    <Button to='github' onMouseEnter={onHover} onMouseLeave={onHover}>GitHub</Button>
                 </MainContent>
-            
-
-
         </MainContainer>
     )
-}
+};
 
 export default MainSection
